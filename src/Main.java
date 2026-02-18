7913284b270819c398fe6d1fae9b1fb9ece20549
commit <ncsakura892@gmail.com>
2026-02-18 10:13:27 +0600

    added sout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+
+
+    System.out.println("Hello Java -21");
     System.out.println("Hello Java -21");
 }
